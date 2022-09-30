b7e3e9a15f6750872d98c89eb4c9320d8749a08b
Natt004 <marlesnataly@gmail.com>
2022-09-30 16:00:51 -0500

    Taller 10

---
+++ Taller10/src/Datos.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Datos {
+private int aneo;
+
+public int getAneo() {
+	return aneo;
+}
+
+public void setAneo(int aneo) {
+	this.aneo = aneo;
+}
+
+}
---
+++ Taller10/src/Principal.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Principal {
+public static void main(String[] args) {	
+Calculo cal=new Calculo();
+cal.CalculoAño();
+ContarNumeros con=new ContarNumeros();
+con.ContarCantidad();
+}
+}
